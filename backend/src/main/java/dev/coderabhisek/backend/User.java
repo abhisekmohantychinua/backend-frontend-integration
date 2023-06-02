b2225a7e36d8f470cf01c75ee90d50fd58913312
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-02 20:55:20 +0530

    Created User entity and implementated by UserDetails

---
+++ backend/src/main/java/dev/coderabhisek/backend/User.java
@@ -0,0 +1,47 @@
+package dev.coderabhisek.backend;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User implements UserDetails {
+    private String username;
+    private String password;
+    private String about;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
