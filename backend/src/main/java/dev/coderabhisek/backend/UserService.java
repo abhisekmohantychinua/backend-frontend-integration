93e8fc03b7af8269cb8772229f85efefca7e50d8
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-06-03 21:28:34 +0530

    Service

--- backend/src/main/java/dev/coderabhisek/backend/UserService.java
+++ backend/src/main/java/dev/coderabhisek/backend/UserService.java
@@ -1,25 +1,21 @@
 package dev.coderabhisek.backend;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository repository;
-    private final long validityInMilliseconds = 60 * 60 * 1000;
-    @Value("${jwt.secretkey}")
-    private final String secretKey;
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+    private final TokenRepository tokenRepository;
 
     public User addUser(User user) {
         return repository.save(user);
@@ -30,23 +26,29 @@ public class UserService {
     }
 
     public User getUser(String username) {
-        return repository
+        var user=repository
                 .findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User name not found!"));
+        System.out.println(username);
+        System.out.println(user);
+        return user;
     }
 
-    public String generateJwtToken(String username) {
-        Claims claims = Jwts.claims().setSubject(username);
-        claims.put("roles", new ArrayList<>());
+    public String authorize(User user) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
+            user = repository.findByUsername(user.getUsername()).orElseThrow(() -> new UsernameNotFoundException(""));
+            String token = jwtUtil.generateToken(user);
+            tokenRepository.addToken(token);
+            System.out.println(token);
+            return token;
 
-        Date now = new Date();
-        Date validity = new Date(now.getTime() + validityInMilliseconds);
-
-        return Jwts.builder()
-                .setClaims(claims)
-                .setIssuedAt(now)
-                .setExpiration(validity)
-                .signWith(SignatureAlgorithm.HS256, secretKey)
-                .compact();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            throw new UsernameNotFoundException("NO user present!");
+        }
     }
+
 }
